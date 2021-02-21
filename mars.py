ae3a2605fb4ddd22cd17a17c60ce63549f2cd314
Vadim-Step <BeJIuKaH1@yandex.ru>
2021-02-22 00:35:18 +0300

    class task 5

--- mars.py
+++ mars.py
@@ -134,5 +134,106 @@ def results(nickname, level, rating):
                 </html>'''
 
 
+@app.route('/selection', methods=['POST', 'GET'])
+def selection():
+    if request.method == 'GET':
+        profs = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                 'инженер по терраформированию', 'климатолог', 'специалист по радиационной защите',
+                 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения', 'метеоролог',
+                 'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов']
+        prof_form = ''
+        for i in profs:
+            prof_form += f'''<div class="form-check">
+                <input class="form-check-input" type="checkBox" name="prof" id="{i}" value="{i}">
+                <label class="form-check-label" for="{i}">
+                  {i}
+                </label>
+                </div>'''
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                          <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Отбор астронавтов</title>
+                          </head>
+                          <body>
+                          <h1><p align="center">Анкета претендента</p></h1>
+                            <h2><p align="center">на участие в миссии</p></h2>
+                            <div>
+                                <form class="login_form" method="post">
+                                    <input name="surname" type="text" class="form-control" placeholder="Введите фамилию"/>
+                                    <input name="name" type="text" class="form-control" placeholder="Введите имя"/>
+                                    </br>
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                                    </br>
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое у вас образование?</label>
+                                        <select>
+                                          <option>Дошкольное</option>
+                                          <option>Начальное общее</option>
+                                          <option>Основное общее</option>
+                                          <option>Среднее</option>
+                                          <option>Среднее профессиональное</option>
+                                          <option>Высшее</option>
+                                        </select>
+                                    </div>
+                                    </br>
+                                    <div class="form-group">
+                                        <label for="classSelect">Какие у вас есть профессии?</label>
+                                        {prof_form}
+                                    </div>
+                                    </br>
+                                    <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    </br>
+                                    <div class="form-group">
+                                        <label for="about">Почему Вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    </br>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    </br>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Записаться</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        print(request.form['surname'])
+        print(request.form['name'])
+        print(request.form['email'])
+        print(request.form['prof'])
+        print(request.form['sex'])
+        print(request.form['about'])
+        print(request.form['file'])
+        return "Форма отправлена"
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
