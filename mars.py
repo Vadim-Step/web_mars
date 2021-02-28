a3cc526dc49cf54e913af749a2c1f5396e3b7db1
Vadim-Step <BeJIuKaH1@yandex.ru>
2021-02-28 17:02:42 +0300

    lesson 2 task 2

--- mars.py
+++ mars.py
@@ -381,5 +381,10 @@ def news(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('index.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
